8055f4e2f40790381956f5d089a7198e20614852
Jared HOUSTON-MCMILLAN <jhouston@c4r19s1.wethinkcode.co.za>
2019-10-16 17:22:56 +0200

    Rush00

--- Rush00/login.php
+++ Rush00/login.php
@@ -1,34 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<link rel = "stylesheet" href = "css/style.css">
+	<link rel = "stylesheet" href = "css/login.css">
 </head>
-<body bgcolor = "white"><!-- 
-	<button onclick="document.getElementById('id01').style.display='block'">Login</button> -->
-	<!-- The Modal -->
-	<div id="id01" class="modal">
-	<span onclick="document.getElementById('id01').style.display='none'"
-
-	<!-- Modal Content -->
-	<form class="modal-content animate" action="/action_page.php">
-		<div class="imgcontainer">
+<body>
+	<header class = "header">
+		<nav class = "nav_links">
+			<ul>
+				<li><a href = "http://localhost:8080/rush00/index.php">Home</a></li>
+				<li><a href = "">Shop</a></li>
+				<li><a href = "">Team</a></li>
+				<li><a href = "">Work</a></li>
+				<li class = "Logo"><a href = ""><img src = "img/Birdy.png" alt = " " class = "White"><img class = "Black" src = "img/Birdy.png" alt = ""></a></li>
+				<li><a href = "">Blog</a></li>
+				<li><a href = "">Contact</a></li>
+				<li><a href = "">Client</a></li>
+				<li><a href = "http://localhost:8080/rush00/login.php">Login</a></li>
+			</ul>
+			<a class = "nav_icon" href = ""><span></span><span></span><span></span></a>
+		</nav>
+	</header>
+	<div id="id01" class="modal"><!-- 
+	<span onclick="document.getElementById('id01').style.display='none'"> -->
+	<form class = "modal-content animate" action="/action_page.php">
+		<div class = "container">
+			<label for = "uname"><b>Username</b></label>
+			<input class = "Fill" type = "text" placeholder = "Enter Username" name = "uname" required>
+			<br>
+			<label for = "psw"><b>Password</b></label>
+			<input class = "Fill" type = "password" placeholder = "Enter Password" name = "psw" required>
+			<br>
+			<button type = "submit">Login</button>
+			<br>
+			<label>
+				<input class = "Remember" type = "checkbox" checked="checked" name="remember">Remember me
+			</label>
 		</div>
-
-		<div class="container">
-		<label for="uname"><b>Username</b></label>
-		<input type="text" placeholder="Enter Username" name="uname" required>
-		<br>
-		<label for="psw"><b>Password</b></label>
-		<input type="password" placeholder="Enter Password" name="psw" required>
-		<button type="submit">Login</button>
-		<label>
-			<input type="checkbox" checked="checked" name="remember"> Remember me
-		</label>
-		</div>
-
-		<div class="container" style="background-color:#f1f1f1">
-		<button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
-		<span class="psw">Forgot <a href="#">password?</a></span>
+		<div class = "container" style = "background-color:#f1f1f1">
+		<button type = "button" onclick = "document.getElementById('id01').style.display = 'none'" class="cancelbtn">Cancel</button>
+		<span class = "psw">Forgot <a href = "#">password?</a></span>
 		</div>
 	</form>
 </div>
