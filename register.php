99d4f3b01eaa9133aa1065546f9579eaf4a7fd42
Jared HOUSTON-MCMILLAN <jhouston@c4r19s1.wethinkcode.co.za>
2019-11-09 08:25:54 +0200

    Move to 5th floor

--- register.php
+++ register.php
@@ -22,14 +22,14 @@
 					<a class = "nav_icon" href = ""><span></span><span></span><span></span></a>
 				</nav>
 			</header>
-			<form action = "./model/input_user.php" method = "POST">
+			<form class = "reg" action = "./model/input_user.php" method = "POST">
 				<table>
 					<div class="container">
-						<tr><td><label class = "">Name</label><input class = "" type="text" name = "firstname" required></td></tr>
-						<tr><td><label class = "">Surname</label><input class = "" type="text" name = "lastname" required></td></tr>
-						<tr><td><label class = "">Username</label><input class = "" type="text" name = "username" required></td></tr>
-						<tr><td><label class = "">E-mail</label><input class = "" type="email" name = "email" required></td></tr>
-						<tr><td><label class = "">Password</label><input class = "" type="password" name = "password_1" required></td></tr>
+						<tr><td><label class = "">Name</label><input class = "" type = "text" name = "firstname" required></td></tr>
+						<tr><td><label class = "">Surname</label><input class = "" type = "text" name = "lastname" required></td></tr>
+						<tr><td><label class = "">Username</label><input class = "" type = "text" name = "username" required></td></tr>
+						<tr><td><label class = "">E-mail</label><input class = "" type = "email" name = "email" required></td></tr>
+						<tr><td><label class = "">Password</label><input class = "" type = "password" name = "password_1" required></td></tr>
 						<tr><td><label class = "">Confirm password</label><input class = "" type="password" name = "password_2" required></td></tr>
 						<tr><td><button type="submit" name="register_btn">Register</button></td></tr>
 						<label>Already have an account?</label>
